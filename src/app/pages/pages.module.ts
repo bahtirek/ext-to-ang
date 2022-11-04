1267c789f344643800c061c4c53a61f5617f7bbe
bahtirek <bahtirek@gmail.com>
2022-11-03 22:49:52 -0400

    created new components

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -5,6 +5,8 @@ import { ReviewPageComponent } from './review-page/review-page.component';
 import { VideoPageComponent } from './video-page/video-page.component';
 import { SettingsPageComponent } from './settings-page/settings-page.component';
 import { ResizeComponent } from '../shared/resize/resize.component';
+import { ElementSelectorComponent } from './report-page/element-selector/element-selector.component';
+import { ScreenCapturerComponent } from './report-page/screen-capturer/screen-capturer.component';
 
 
 
@@ -14,7 +16,9 @@ import { ResizeComponent } from '../shared/resize/resize.component';
     ReviewPageComponent,
     VideoPageComponent,
     SettingsPageComponent,
-    ResizeComponent
+    ResizeComponent,
+    ElementSelectorComponent,
+    ScreenCapturerComponent
   ],
   imports: [
     CommonModule
---
+++ src/app/pages/report-page/screen-capturer/screen-capturer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-screen-capturer',
+  templateUrl: './screen-capturer.component.html',
+  styleUrls: ['./screen-capturer.component.less']
+})
+export class ScreenCapturerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
