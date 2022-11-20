ce206a1b785bd097d7dac15712143441ed684c70
bahtirek <bahtirek@gmail.com>
2022-11-19 19:38:22 -0500

    module saerch component

---
+++ src/app/shared/module-search/module-search.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-module-search',
+  templateUrl: './module-search.component.html',
+  styleUrls: ['./module-search.component.less']
+})
+export class ModuleSearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
